7911b8a277684305fc4e11eb1035935a1ec94868
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-02-28 08:28:59 -0800

    don't need questionLikeForm

--- app/forms/question_like_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-
-from wtforms import IntegerField
-from wtforms.validators import DataRequired
-
-class QuestionForm(FlaskForm):
-  user_id = IntegerField("user_id", validators=[DataRequired()])
-  question_id = IntegerField("question_id", validators=[DataRequired()])
-  like_unlike = IntegerField("like_unlike", validators=[DataRequired()])
