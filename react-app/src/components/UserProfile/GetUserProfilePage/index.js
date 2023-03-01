c170eb09597f36ed3344b921106f280d6d11a162
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-03-01 15:15:18 -0800

    userProfile functional down

--- react-app/src/components/UserProfile/GetUserProfilePage/index.js
+++ react-app/src/components/UserProfile/GetUserProfilePage/index.js
@@ -11,6 +11,8 @@ export default function UserProfilePage(){
   const {userId} = useParams()
   const [isLoaded, setIsLoaded] = useState(false)
 
+  // const options = { year: 'numeric', month: 'long', day:'numeric' };
+  const options = { year: 'numeric', month: 'long', day:'numeric' };
   const variable = useSelector(state => {
     console.log("state from userProfile", state)
   })
@@ -26,7 +28,27 @@ export default function UserProfilePage(){
   })
 
   const questions = currentProfileUserQuestions? Object.values(currentProfileUserQuestions):[]
-  console.log("real questions from userProfile", Array.isArray(questions), questions)
+  console.log("real questions from userProfile", Array.isArray(questions[0]), questions)
+
+  let questionSession
+  if (Array.isArray(questions[0])){
+    questionSession = (
+      <div>{questions[0][0]}</div>
+    )
+  }else{
+    questionSession = (
+      <div>
+        {questions.map(question => (
+              <div key={question.id}>
+                <div>{question.like_count} likes</div>
+                <Link to={`/questions/${question.id}`}> {question.title}</Link>
+                <div>{new Date(question.updatedAt).toLocaleDateString("en-US", options)}</div>
+              </div>
+            ))}
+      </div>
+    )
+  }
+
 
   const currentProfileUserAnswers = useSelector(state => {
     console.log("answers from userProfile", state.userProfile.answers)
@@ -36,6 +58,25 @@ export default function UserProfilePage(){
   const answers = currentProfileUserAnswers? Object.values(currentProfileUserAnswers):[]
   console.log("real answers from userProfile", Array.isArray(answers), answers)
 
+  let answerSession
+  if (Array.isArray(answers[0])){
+    answerSession = (
+      <div>{answers[0][0]}</div>
+    )
+  }else{
+    answerSession = (
+      <div>
+        {answers.map(answer => (
+          <div key={answer.id}>
+            <div>{answer.like_count} likes</div>
+            <Link to={`/questions/${answer.question.id}`}>{answer.question.title}</Link>
+            <div>{new Date(answer.updatedAt).toLocaleDateString("en-US", options)}</div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   useEffect(() => {
     dispatch(fetchUserProfileInfo(userId)).then(() => {
       return setIsLoaded(true)
@@ -43,7 +84,6 @@ export default function UserProfilePage(){
   }, [dispatch])
 
 
-  const options = { year: 'numeric', month: 'long', day:'numeric' };
 
   return (
     isLoaded && (
@@ -57,28 +97,15 @@ export default function UserProfilePage(){
           <div>{currentProfileUser.username}</div>
           <div>{new Date(currentProfileUser.createdAt).toLocaleDateString("en-US", options)}</div>
         </div>
-
         </div>
         <div className="answers-questions">
           <div className="questions">
             <h3>All questions</h3>
-            {questions.map(question => (
-              <div key={question.id}>
-                <div>{question.like_count} likes</div>
-                <Link to={`/questions/${question.id}`}> {question.title}</Link>
-                <div>{new Date(question.updatedAt).toLocaleDateString("en-US", options)}</div>
-              </div>
-            ))}
+            {questionSession}
           </div>
           <div className="answers">
-              <h3>All answers</h3>
-              {answers.map(answer => (
-                <div key={answer.id}>
-                  <div>{answer.like_count} likes</div>
-                  <Link to={`/questions/${answer.question.id}`}>{answer.question.title}</Link>
-                  <div>{new Date(answer.updatedAt).toLocaleDateString("en-US", options)}</div>
-                </div>
-              ))}
+            <h3>All answers</h3>
+            {answerSession}
           </div>
         </div>
       </div>
