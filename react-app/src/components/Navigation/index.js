fce33db22055e8ad68405f0ff0b852f1efb3d412
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-03-02 08:29:19 -0800

    modify favicon

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -11,10 +11,10 @@ function Navigation({ isLoaded }){
 		<ul className='navigation'>
 			<div className='icon-menu'>
 				<li className="icon-list">
-					<NavLink exact to="/"><img src='https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg'/></NavLink>
+					<NavLink exact to="/"><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR71-Pz4ApR2-rKpzs1UQg7shcX3N0-BLiTYfjs6ZGvFu-Zv6e1ennmYeznXxGVASzpXBQ&usqp=CAU'/></NavLink>
 				</li>
 				<li className="menu-list">
-					<NavLink exact to="/">Questions</NavLink>
+					<NavLink exact to="/">All Questions</NavLink>
 				</li>
 			</div>
 
